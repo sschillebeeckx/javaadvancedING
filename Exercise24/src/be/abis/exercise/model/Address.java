29ba69bb465626c329db202115bf9f6bdfb82c58
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2022-12-06 15:51:25 +0100

    exercise24

---
+++ Exercise24/src/be/abis/exercise/model/Address.java
@@ -0,0 +1,22 @@
+package be.abis.exercise.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor @AllArgsConstructor @Getter @Setter
+public class Address {
+	
+	private String street;
+	private String nr;
+	private String zipCode;
+	private String town;
+	private String country;
+	private String countryCode;
+
+	@Override
+	public String toString() {
+		return street + " " + nr + ", " + zipCode + " " + town + ", " + country + " (" + countryCode +")";
+	}
+}
---
+++ Exercise24/src/be/abis/exercise/test/TestMain.java
@@ -0,0 +1,54 @@
+package be.abis.exercise.test;
+
+import be.abis.exercise.exception.PersonAlreadyExistsException;
+import be.abis.exercise.model.Address;
+import be.abis.exercise.model.Company;
+import be.abis.exercise.model.Person;
+import be.abis.exercise.repository.FilePersonRepository;
+import be.abis.exercise.repository.PersonRepository;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        try {
+            PersonRepository pr = FilePersonRepository.getInstance();
+            List<Person> persons= pr.getAllPersons();
+
+            System.out.println("All persons in file");
+            persons.forEach(System.out::println);
+
+            //Adding person
+            Address a = new Address("Diestsevest","32 bus 4b","3000","Leuven","België","B");
+            Company c = new Company("Abis",a);
+            Person p1 = new Person("Emily","Mees", LocalDate.of(1990,02,5),"emees@abis.be","abis345",c);
+            // checking toString via @Data
+            System.out.println(p1.getCompany());
+            pr.addPerson(p1);
+
+            //Adding person without company
+            Person p2 = new Person("John","Doe", LocalDate.of(1971,01,18),"john.doe@gmail.com","mypassword");
+            pr.addPerson(p2);
+
+            System.out.println("\nAll persons in file after adding");
+            pr.getAllPersons().forEach(System.out::println);
+
+            System.out.println("\nAdding existing person should throw exception");
+            Person p3 = new Person("Sandy","Schillebeeckx", LocalDate.of(1978,04,10),"sschillebeeckx@abis.be","abis123");
+            try {
+                pr.addPerson(p3);
+            } catch (PersonAlreadyExistsException paee){
+                System.out.println(paee.getMessage());
+            }
+
+
+        } catch (IOException | PersonAlreadyExistsException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+}
